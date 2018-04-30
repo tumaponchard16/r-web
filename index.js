1f10606ce189f8b0631aa72229ff94c75ba6260f
Richard <tumaponchard16@gmail.com>
2018-05-01 03:24:10 +0800

    configure to deployment

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.post('/api/form', (req, res) => {
     nodemailer.createTestAccount((err, account) => {
